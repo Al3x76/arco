011bc11df62cf917017a5f4d842bdd547ab19d55
Pompiliu Alexoiu <alexoiupompiliu@gmail.com>
2019-06-25 01:14:03 +0300

    display manager

---
+++ 100-display-manager-and-desktop-v1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Pompiliu Alexoiu
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+echo "#################################################"
+echo "If it feels like the downloads are too slow"
+echo "Stop the installation with CTRL + C"
+echo "and run the alias - mirror in the terminal"
+echo "#################################################"
+
+sudo pacman -Syyu --noconfirm
+#installing displaymanager or login manager
+sudo pacman -S --noconfirm --needed lightdm
+sudo pacman -S --noconfirm --needed arcolinux-lightdm-gtk-greeter arcolinux-lightdm-gtk-greeter-settings
+sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git 
+#installing desktop environment
+sudo pacman -S gnome --noconfirm --needed
+#sh AUR-DS/install-budgie-desktop-git-v*.sh
+sudo pacman -S budgie-desktop --noconfirm --needed
+#enabling displaymanager or login manager
+sudo systemctl enable lightdm.service -f
+sudo systemctl set-default graphical.target
+
+
+#Remove anything you do not like from the installed applications
+
+#sudo pacman -R ...
